f9c48d91343f9741b0b47558d85a7576a17c9b5b
Dilraj Sandhu <49254698+DilrajS@users.noreply.github.com>
2024-02-22 00:03:44 -0800

    Fixed Signin/Signout

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -26,23 +26,43 @@ export default function Home() {
               <button className="AppButton">Sign in</button>
             </SignInButton>
           </SignedOut>
-          <SignInButton>
-            <SignedOut>
+
+          <SignedIn>
+            <SignOutButton>
               <button className="AppButton">Log off</button>
-            </SignedOut>
-          </SignInButton>
+            </SignOutButton>
+          </SignedIn>
         </div>
       </nav>
       <div className="head-text"></div>
       <section id="home" className="sectionD cta">
-        <h1 className="HomeHeading">Generate Your Flashcards</h1>
+        <h1 className="HomeHeading">AI Generate Flashcards</h1>
         <p className="HomePara">
           Using the power of AI, start creating your personalized flashcards
           today and enhance your learning experience!
         </p>
-        <Link href={"/generate"}>
-          <button className="ctaButton"> Try now! </button>
-        </Link>
+        {/* Get Started (CTA) Button */}
+        <div class="button-container">
+          <Link href="/generate">
+            <button class="cssbuttons-io-button">
+              Get started
+              <div class="icon">
+                <svg
+                  height="24"
+                  width="24"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+            </button>
+          </Link>
+        </div>
       </section>
       <section id="about" className="sectionL">
         <h1 className="HomeHeading">About Us</h1>
